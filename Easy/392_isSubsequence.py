26fd7b0f6605ec6094d0ecdfde4692c9eba3be61
TanatB <bright.tanat@hotmail.com>
2024-11-14 11:13:37 +1000

    Solved 392 with 27% runtime beat, 80% space complexity beat

--- Easy/392_isSubsequence.py
+++ Easy/392_isSubsequence.py
@@ -1,6 +1,18 @@
+# Two pointers
 def isSubsequence(s: str, t: str) -> bool:
-    pass
-
+    one, two = 0, 0
+    
+    while one < len(s) and two < len(t):
+        if s[one] == t[two]:
+            one += 1
+        two += 1
+    
+    if one == len(s):
+        return True
+    
+    return False
 
 if __name__ == "__main__":
     print("this is Subsequence question")
+    s, t = "aaaaaa", "bbaaaa"
+    print(isSubsequence(s, t))
